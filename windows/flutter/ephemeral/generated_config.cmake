e34ae3c8c7263bdede3e68348cca57bcc6db87b5
Dhairya Dudakiya <dhairya.dudakiya121451@marwadiuniversity.ac.in>
2025-02-03 22:04:29 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\MU\\Sem 6\\Mini Project\\onlineexamsystem" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\MU\\Sem 6\\Mini Project\\onlineexamsystem"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\MU\\Sem 6\\Mini Project\\onlineexamsystem\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\MU\\Sem 6\\Mini Project\\onlineexamsystem"
+  "FLUTTER_TARGET=C:\\MU\\Sem 6\\Mini Project\\onlineexamsystem\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\MU\\Sem 6\\Mini Project\\onlineexamsystem\\.dart_tool\\package_config.json"
+)
